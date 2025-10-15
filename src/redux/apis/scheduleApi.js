254e290a51b778d2b4d79a4ec40e4d4850feded6
unknown <bodkhesamadhan188@gmail.com>
2025-10-15 09:13:48 +0530

    update schedule api

--- src/redux/apis/scheduleApi.js
+++ src/redux/apis/scheduleApi.js
@@ -2,10 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const scheduleApi = createApi({
     reducerPath: "scheduleApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/schedule`,
-        credentials: "include",
-    }),
+  baseQuery: fetchBaseQuery({
+  baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/schedule`,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    const token = localStorage.getItem("token");
+    if (token) headers.set("authorization", `Bearer ${token}`);
+    return headers;
+  },
+}),
+
     tagTypes: ["Schedule"],
     endpoints: (builder) => ({
         getTeacherSchedule: builder.query({
